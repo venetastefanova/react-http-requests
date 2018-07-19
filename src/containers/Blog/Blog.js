70db3e19659ef5111b3f29e16abca24515093aa0
Veneta Stefanova <venetastefanovaa@gmail.com>
2018-07-19 12:31:16 +0300

    creating a HTTP request with axios and updating the state dynamically from the response

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state= {
+        posts: []
+    }
+    //this method is the right place to do AJAX call
+    componentDidMount(){
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+        .then(response=>{
+            this.setState({posts:response.data}); // changing the state after the return of the promise
+            return console.log(response);
+        });
+    }
     render () {
+
+        const posts = this.state.posts.map(post=>{
+            return <Post key={post.id} title={post.title}/>
+        })
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
