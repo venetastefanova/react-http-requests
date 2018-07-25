bd77ef002b7f04e08b44e1c0c699e67170cc989d
Veneta Stefanova <venetastefanovaa@gmail.com>
2018-07-25 13:27:57 +0300

    rendering the posts component for the /home router

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
+import { Route } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -16,8 +17,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts/>
-                
+                <Route path="/" exact component={Posts}/>
             </div>
         );
     }
