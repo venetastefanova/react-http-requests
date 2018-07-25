87b6a048666c0bccb68a1e92339dace97e31ade1
Veneta Stefanova <venetastefanovaa@gmail.com>
2018-07-25 13:04:38 +0300

    restructuring and preparing for routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,65 +2,23 @@ import React, { Component } from 'react';
 // import axios from 'axios';
 import axios from '../../axios'; // gets the new instance created
 import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import Posts from './Posts/Posts';
 
 class Blog extends Component {
-    state= {
-        posts: [],
-        selectedPostId: null,
-        error:false
-    }
-    //this method is the right place to do AJAX call
-    componentDidMount(){
-        axios.get("/posts")
-        .then(response=>{
-            //gets the first 4 posts
-            const posts = response.data.slice(0,4);
-            const updatedPosts = posts.map(post=>{
-                return {
-                    ...post, // distributes the properties of the post
-                    author: 'Veneta' // and adding a new one
-                }
-            });
-            this.setState({posts:updatedPosts}); // changing the state after the return of the promise
-           // return console.log(response);
-        })
-        .catch(error=>{
-            console.log(error);
-          this.setState({error:true});//changing the state and setting sthe error to true
-        });
-    }
-
-    postSelectedHandler = (id) => {
-         this.setState({selectedPostId: id});
-    }
-    render () {
-            let posts = <p style={{textAlign:'center'}}>Woops, something went wrong!</p>;
-            
-            if(!this.state.error){
-            
-                posts = this.state.posts.map(post=>{
-                    return <Post 
-                            key={post.id} 
-                            title={post.title} 
-                            author={post.author}
-                            clicked={()=>this.postSelectedHandler(post.id)}/>;
-            });
-        }
-        
+    render () {     
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                <Posts/>
+                
             </div>
         );
     }
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import axios from '../../../axios'; // import our own instance
+import Post from '../../../components/Post/Post';
+import './Posts.css';
+
+class Posts extends Component {
+    state= {
+        posts: []
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+   }
+    
+    //this method is the right place to do AJAX call
+    componentDidMount(){
+        axios.get("/posts")
+        .then(response=>{
+            //gets the first 4 posts
+            const posts = response.data.slice(0,4);
+            const updatedPosts = posts.map(post=>{
+                return {
+                    ...post, // distributes the properties of the post
+                    author: 'Veneta' // and adding a new one
+                }
+            });
+            this.setState({posts:updatedPosts}); // changing the state after the return of the promise
+           // return console.log(response);
+        })
+        .catch(error=>{
+            console.log(error);
+          //this.setState({error:true});//changing the state and setting sthe error to true
+        });
+    }
+    render(){
+        let posts = <p style={{textAlign:'center'}}>Woops, something went wrong!</p>;
+        
+        if(!this.state.error){
+        
+            posts = this.state.posts.map(post=>{
+                return <Post 
+                        key={post.id} 
+                        title={post.title} 
+                        author={post.author}
+                        clicked={()=>this.postSelectedHandler(post.id)}/>;
+        });
+    }
+        return(
+            <section className="Posts">
+                    {posts}
+             </section>
+        );
+    }
+}
+
+export default Posts;
