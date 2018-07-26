a729d4c977676839845578ca9bea1f8154579dc2
Veneta Stefanova <venetastefanovaa@gmail.com>
2018-07-26 10:56:11 +0300

    adding {switch} from react-router-dom and making sure the routes execute properly

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink,Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -27,8 +27,11 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Route path="/" exact component={Posts}/>
-                <Route path="/new-post" exact component={NewPost}/>
-                <Route path="/:id" component={FullPost}/>
+                <Switch>
+                    <Route path="/new-post" exact component={NewPost}/>
+                    <Route path="/:id" component={FullPost}/>
+                </Switch>
+                
             </div>
         );
     }
