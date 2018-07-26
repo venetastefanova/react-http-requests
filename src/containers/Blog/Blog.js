40abb024123001e47615dcba22547fcaa7d271fe
Veneta Stefanova <venetastefanovaa@gmail.com>
2018-07-26 09:54:32 +0300

    styling active link of navigation

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -13,12 +13,15 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
-                                pathname: this.props.match.url + '/new-post',
+                            <li><NavLink 
+                                to="/"
+                                exact
+                                activeClassName="my-active">Home</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
                                 hash:'#submit',
                                 search: '?quick-submit=true'
-                            }}>New</Link></li>
+                            }} exact>New</NavLink></li>
                         </ul>
                     </nav>
                 </header>
